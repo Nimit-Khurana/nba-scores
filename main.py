e93ae36c0668db53d8d24797b74f19e9d116fdfb
Nimit <nimbukhur@gmail.com>
2019-09-14 00:40:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,14 @@
-from bs4 import BeautifulSoup as soup
-import requests
-
-date = input("Enter date to look for(YYYYMMDD): ")
-base_url = "http://www.espn.in/nba/schedule"
-dated_url = base_url + "/_/date/" + date
-
-url_content = requests.get(dated_url)
-team_attrs = {
-    "Atlanta Hawks": "ATL",
-    "Boston Celtics": "BOS",
-    "Brooklyn Nets" : "BKN",
-    "Charlotte Hornets" : "CHA",
-    "Chicago Bulls" : "CHI",
-    "Cleveland Cavaliers" : "CLE",
-    "Dallas Mavericks" : "DAL",
-    "Denver Nuggets" : "DEN",
-    "Detroit Pistons" : "DET",
-    "Golden State Warriors" : "GS",
-    "Houston Rockets" : "HOU",
-    "Indiana Pacers" : "IND",
-    "LA Clippers" : "LAC",
-    "Los Angeles Lakers" : "LAL",
-    "Memphis Grizzles" : "MEM",
-    "Miami Heat" : "MIA",
-    "Milwaukee Bucks" : "MIL",
-    "Minnesota Timberwolves" : "MIN",
-    "New Orleanas Pelicans" : "NO",
-    "New York Knicks" : "NY",
-    "Oklohoma City Thunder" : "OKC",
-    "Orlando Magic" : "ORL",
-    "Philadelphia 76ers" : "PHI",
-    "Phoenix Suns" : "PHX",
-    "Portland Trail Blazers" : "POR",
-    "Sacremanto Kings" : "SAC",
-    "San Antonio Spurs" : "SA",
-    "Toronto Raptors" : "TOR",
-    "Utah Jazz" : "UTAH",
-    "Washington Wizards" : "WSH"
-}
+from scores import scores, standings, teams
 
 
-if url_content.status_code == requests.codes.ok:
-    print ("Response OK")
+def main():
+    user = input("[s]cores | St[a]ndings | [t]eams: ")
+    if user == "s":
+        scores()
+    elif user == "a":
+        standings()
+    elif user == "t":
+        teams()
 
-schedule_container = soup(url_content.text, "html.parser")
-
-date = schedule_container.findAll("h2")[0]
-print (date.contents[0])
-print ("\n")
-
-# next tag after h2
-# the table schedule
-table_sibling = date.next_sibling
-
-for i in range(1,len(table_sibling.table.findAll("tr"))):
-    for tr_data in table_sibling.table.findAll("tr")[i].findAll("a", {"name" : "&lpos=nba:schedule:score"}):
-        print (tr_data.contents[0].replace(",", " beat"))
-
-print ("Matches Played: {}".format(len(table_sibling.table.findAll("tr"))-1))
-print ("\n----Team's attributes----")
-for keyss,valuess in team_attrs.items():
-    print (valuess + " -- " + keyss)
+if __name__ == "__main__":
+    main()
